6c82fccd10bcd079c46241592c78298784797093
Bruna Fernandes <brunafernandes.idka@gmail.com>
2024-04-17 08:04:53 -0300

    refactor: unit test app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,21 +11,36 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
+    // Arrange
     const fixture = TestBed.createComponent(AppComponent);
+
+    // Act
     const app = fixture.componentInstance;
+
+    // Assert
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Smart Fit'`, () => {
+    // Arrange
     const fixture = TestBed.createComponent(AppComponent);
+
+    // Act
     const app = fixture.componentInstance;
+
+    // Assert
     expect(app.title).toEqual('Smart Fit');
   });
 
-  it('should render title', () => {
+  it('should render the app-home component', () => {
+    // Arrange
     const fixture = TestBed.createComponent(AppComponent);
+
+    // Act
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+
+    // Assert
     expect(compiled.querySelector('app-home')).toBeTruthy();
   });
 });
